078d301c6d683221542dfab02778b5797bbb0ede
vbekarev <vbekarev@yandex.ru>
2018-04-24 23:10:42 +0700

    add Person

--- src/main/java/ru/vbekarev/git/Person.java
+++ src/main/java/ru/vbekarev/git/Person.java
@@ -4,4 +4,20 @@ package ru.vbekarev.git;
  * Created by v.bekarev on 24.04.2018.
  */
 public class Person {
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                '}';
+    }
 }
